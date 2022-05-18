87f7b986711d88d0775086e2cd5cf96053c6aee6
ldvy <hardway.studio@gmail.com>
2022-05-18 23:59:16 +0300

    feat: add js test

---
+++ jest/references/js/transform.js
@@ -0,0 +1,45 @@
+var transform =
+    [
+        {
+            "source": "National government reports",
+            "count": 4,
+            "percents": "33%"
+        },
+        {
+            "source": "COVID-19 Data Repository by the Center for Systems Science and Engineering (CSSE) at Johns Hopkins University",
+            "count": 2,
+            "percents": "17%"
+        },
+        {
+            "source": "International Organization for Standardization",
+            "count": 1,
+            "percents": "8%"
+        },
+        {
+            "source": "Our World in Data",
+            "count": 1,
+            "percents": "8%"
+        },
+        {
+            "source": "World Bank World Development Indicators, sourced from Food and Agriculture Organization and World Bank estimates",
+            "count": 1,
+            "percents": "8%"
+        },
+        {
+            "source": "World Bank World Development Indicators, sourced from International Diabetes Federation, Diabetes Atlas",
+            "count": 1,
+            "percents": "8%"
+        },
+        {
+            "source": "European CDC for European countries / UK Government / HHS for the United States / COVID-19 Tracker for Canada",
+            "count": 1,
+            "percents": "8%"
+        },
+        {
+            "source": "United Nations Development Programme (UNDP)",
+            "count": 1,
+            "percents": "8%"
+        }
+    ]
+
+module.exports = transform;
